f7b1fa0c52145b29f35d9252dc77bc7a15a0993b
Anton Okhmat <anton.okhmat@enkesystems.com>
2023-11-17 03:07:56 -0800

    init 2

--- lib/db.py
+++ lib/db.py
@@ -6,6 +6,7 @@ WAREHOUSE = os.getenv('WAREHOUSE')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
+
 # con = snow.connect(
 #     user='XXXX',
 #     password='XXXX',
