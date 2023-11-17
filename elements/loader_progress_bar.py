c58bb5eb8287dcbb7023f061038e4372e6ef253e
Anton Okhmat <anton.okhmat@enkesystems.com>
2023-11-17 03:18:14 -0800

    init 2

--- elements/loader_progress_bar.py
+++ elements/loader_progress_bar.py
@@ -6,18 +6,18 @@ class LoadBarProgress:
     def run_progress(self):
         with st.container():
             load_text = st.empty()
-            # load_text.text('Starting a long computation...')
-            #
-            # # Add a placeholder
-            # latest_iteration = st.empty()
-            # bar = st.progress(0)
-            #
-            # for i in range(100):
-            #     # Update the progress bar with each iteration.
-            #     latest_iteration.text(f'Iteration {i + 1}')
-            #     bar.progress(i + 1)
-            #     # time.sleep(0.01)
-            #
-            # bar.empty()
-            # latest_iteration.empty()
-            # load_text.empty()
+            load_text.text('Starting a long computation...')
+
+            # Add a placeholder
+            latest_iteration = st.empty()
+            bar = st.progress(0)
+
+            for i in range(100):
+                # Update the progress bar with each iteration.
+                latest_iteration.text(f'Iteration {i + 1}')
+                bar.progress(i + 1)
+                # time.sleep(0.01)
+
+            bar.empty()
+            latest_iteration.empty()
+            load_text.empty()
